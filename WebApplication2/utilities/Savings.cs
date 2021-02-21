9a6fe419744f3af0e60642e500b39d49e4620d6b
qaweey Soetan <qaweey.soetan@thebulb.africa>
2021-02-21 22:49:52 +0100

    Add project files.

---
+++ WebApplication2/utilities/Savings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.utilities
+{
+    public class Savings
+    {
+
+
+       public decimal InterestCalculator(decimal p, decimal r, decimal t)
+        {
+            if (p==1)
+            {
+                return 1;
+
+            }
+            else  if (p<0)
+            {
+                return 0;
+
+            }
+            else
+            {
+                return (p * r * t / 100) + p;
+            }
+
+           
+        }
+    }
+}
